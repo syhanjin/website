1d73bbc154fcdd58cf494069b668d46740df3e4a
syhanjin <2819469337@qq.com>
2021-08-05 17:19:16 +0800

    audio

--- index_443.py
+++ index_443.py
@@ -1,6 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 from flask import Flask,request,render_template, session,redirect,make_response,send_file #引入flask
-from handler import getdatas,register,login,pwa,user,games,tool_EL,admin_file,c18,blog,photo,chat #,special_res
+from handler import getdatas,register,login,pwa,user,games,tool_EL,admin_file,c18,blog,photo,audio #chat,special_res
 import socket,base64,datetime
 from datetime import timedelta
 def get_host_ip():
@@ -119,6 +119,7 @@ sy.register_blueprint(c18.c18m,url_prefix='/c18/m')
 sy.register_blueprint(blog.blog,url_prefix='/blog')
 sy.register_blueprint(blog.blogm,url_prefix='/m/blog')
 sy.register_blueprint(photo.photo,url_prefix='/photo')
+sy.register_blueprint(audio.audio,url_prefix='/audio')
 # sy.register_blueprint(special_res.sr,url_prefix='/res')
 # sy.register_blueprint(chat.chat,url_prefix='/chat') # 聊天室暂关
 
