37fc1439b6b1840a113104c8710c4111086bb1b9
syhanjin <2819469337@qq.com>
2021-08-07 10:25:58 +0800

    user-display-记录点5

--- handler/user.py
+++ handler/user.py
@@ -1,63 +1,94 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, request,jsonify,session,redirect,make_response
-import pymongo,random,datetime,re,os,hashlib
+from flask import Blueprint, render_template, request, jsonify, session, redirect, make_response
+import pymongo
+import random
+import datetime
+import re
+import os
+import hashlib
 import base64
-client = pymongo.MongoClient('127.0.0.1',27017)
+client = pymongo.MongoClient('127.0.0.1', 27017)
 userdb = client['user']
 maindb = client['main']
 noveldb = client['novel']
 userb = Blueprint('user', __name__)
-usermb = Blueprint('userm',__name__)
+usermb = Blueprint('userm', __name__)
+
+
 def getuser(_uid):
     if _uid and not session.get('_uid') == _uid:
         return None
     return _uid
 # 页面
-## 电脑版
+# 电脑版
+
+
 @userb.route('/settings')
 def user_settings():
     _uid = getuser(request.cookies.get('_uid'))
     if _uid == None:
         return redirect('/login')
     return render_template('/user/pc/settings.html')
+
+
 @userb.route('/<string:_uid>')
 def user_display(_uid):
-    data = userdb.userdata.find_one({'_uid':_uid})
+    data = userdb.userdata.find_one({'_uid': _uid})
     if data == None:
-        return render_template('error/pc.html',error='找不到用户：uid='+_uid)
+        return render_template('error/pc.html', error='找不到用户：uid='+_uid)
     data['is_mine'] = (_uid == data['_uid'])
     # 获取等级信息
-    lvld=userdb.lvldata.find_one({'lvl':data['lvl']})
-    data['max_exp']=lvld['exp']
-    return render_template('user/pc/display.html',data=data)
+    lvld = userdb.lvldata.find_one({'lvl': data['lvl']})
+    data['max_exp'] = lvld['exp']
+    return render_template('user/pc/display.html', data=data)
 
-## 手机版
+# 手机版
+
+
 @usermb.route('/settings')
 def user_m_settings():
     _uid = getuser(request.cookies.get('_uid'))
     if _uid == None:
         return redirect('/login')
     return render_template('/user/m/settings.html')
+
+
 @usermb.route('/<string:_uid>')
 def user_m_display(_uid):
-    data = userdb.userdata.find_one({'_uid':_uid})
+    data = userdb.userdata.find_one({'_uid': _uid})
     if data == None:
-        return render_template('error/m.html',error='找不到用户：uid='+_uid)
+        return render_template('error/m.html', error='找不到用户：uid='+_uid)
     data['is_mine'] = (_uid == data['_uid'])
-    return render_template('user/m/display.html',data=data)
+    return render_template('user/m/display.html', data=data)
 
 # 操作
-@userb.route('/modify/personalized',methods=['POST'])
+
+
+@userb.route('/modify/personalized', methods=['POST'])
 def user_modify_pres():
     _uid = getuser(request.cookies.get('_uid'))
     text = request.form.get('text')
     if _uid == None or text == None:
         return 'False'
-    userdb.userdata.update_one({'_uid':_uid},{'$set':{'personalized':text}})
+    userdb.userdata.update_one(
+        {'_uid': _uid}, {'$set': {'personalized': text}})
     return 'True'
 
 
-@userb.route('/settings/uplphoto',methods=['POST'])
+@userb.route('/modify/introduction', methods=['POST'])
+def user_modify_intr():
+    _uid = getuser(request.cookies.get('_uid'))
+    md = request.form.get('md')
+    html = request.form.get('html')
+    if _uid == None or md == None or html == None:
+        return 'False'
+    userdb.userdata.update_one({'_uid': _uid}, {'$set': {
+        'introduction': {'md': md, 'html': html}
+    }})
+    return 'True'
+
+
+@userb.route('/settings/uplphoto', methods=['POST'])
 def user_settings_uplphoto():
     _uid = getuser(request.cookies.get('_uid'))
     if _uid == None:
@@ -66,44 +97,51 @@ def user_settings_uplphoto():
     # 将base64转为图片文件
     img = base64.b64decode(dataURL)
     # 按时间生成随机文件名
-    fn = 'static/photos/'+datetime.datetime.now().__format__('%Y%m%d%H%M%S')+str(random.randint(100,999))+'.jpg'
-    with open(fn,'wb') as f:
+    fn = 'static/photos/'+datetime.datetime.now().__format__('%Y%m%d%H%M%S') + \
+        str(random.randint(100, 999))+'.jpg'
+    with open(fn, 'wb') as f:
         f.write(img)
     # 删除原本文件
-    lp = userdb.userdata.find_one({'_uid':_uid})['photo']
-    if '/static/photos' in lp: # 判断是否存在/static/photos
+    lp = userdb.userdata.find_one({'_uid': _uid})['photo']
+    if '/static/photos' in lp:  # 判断是否存在/static/photos
         try:
             os.remove(lp[1:])
         except:
             pass
-    userdb.userdata.update_one({'_uid':_uid},{'$set':{'photo':'/'+fn}})
+    userdb.userdata.update_one({'_uid': _uid}, {'$set': {'photo': '/'+fn}})
     return 'True'
-@userb.route('/settings/setuser',methods=['POST'])
+
+
+@userb.route('/settings/setuser', methods=['POST'])
 def user_settings_setuser():
     _uid = getuser(request.cookies.get('_uid'))
     if _uid == None:
         return 'Not Logged In'
     nuser = request.form.get('user')
-    if not userdb.userdata.find_one({'user':nuser}) == None:
+    if not userdb.userdata.find_one({'user': nuser}) == None:
         return 'Existed'
-    data = userdb.userdata.find_one({'_uid':_uid})
+    data = userdb.userdata.find_one({'_uid': _uid})
 #     print(data)
     if 'umodifydate' not in data or (datetime.datetime.now() - data['umodifydate']).days > 365:
-        userdb.userdata.update_one({'_uid':_uid},{'$set':{'user':nuser,'umodifydate':datetime.datetime.now()}})
+        userdb.userdata.update_one({'_uid': _uid}, {
+                                   '$set': {'user': nuser, 'umodifydate': datetime.datetime.now()}})
         return 'True'
     return 'False'
-@userb.route('/settings/pwdmodify',methods=['POST'])
+
+
+@userb.route('/settings/pwdmodify', methods=['POST'])
 def user_settings_modify_pwd():
     _uid = getuser(request.cookies.get('_uid'))
     if _uid == None:
         return 'Not Logged In'
     old = request.form.get('old')
     oldmd5 = hashlib.md5(old.encode(encoding='UTF-8')).hexdigest()
-    data = userdb.userdata.find_one({'_uid':_uid})
+    data = userdb.userdata.find_one({'_uid': _uid})
     if not data['pwd'] == oldmd5:
         return 'pwd wrong'
     new = request.form.get('new')
     newmd5 = hashlib.md5(new.encode(encoding='UTF-8')).hexdigest()
-    session['utime']=datetime.datetime.now().__format__('%Y-%m-%d %H:%M:%S')
-    userdb.userdata.update_one({'_uid':_uid},{'$set':{'pwd':newmd5,'pmodify':session['utime']}})
+    session['utime'] = datetime.datetime.now().__format__('%Y-%m-%d %H:%M:%S')
+    userdb.userdata.update_one(
+        {'_uid': _uid}, {'$set': {'pwd': newmd5, 'pmodify': session['utime']}})
     return 'True'
--- static/js/user/pc/display.js
+++ static/js/user/pc/display.js
@@ -1,4 +1,50 @@
+
+function HTMLDecode(text) {
+    var temp = document.createElement("div");
+    temp.innerHTML = text;
+    var output = temp.innerText || temp.textContent;
+    temp = null;
+    return output;
+}
+var code_copy = function (e) {
+    var t = e.target || e.srcElement;
+    if (t.className.indexOf('hljs-button') > -1) {
+        e.preventDefault();
+        var i = document.getElementById("hljs-copy-el"), n = document.documentElement.scrollTop;
+        i || (i = document.createElement("textarea"),
+            i.style.position = "absolute",
+            i.style.left = "-9999px",
+            i.style.top = n + "px",
+            i.id = "hljs-copy-el",
+            document.body.appendChild(i)),
+            i.textContent = e.currentTarget.innerText.replace(/[\u00A0]/gi, " ");
+        i.select();
+        try {
+            var r = document.execCommand("copy");
+            t.dataset.title = r ? '复制成功' : '复制失败', r && setTimeout(function () {
+                t.dataset.title = '复制'
+            }, 1e3)
+        } catch (a) {
+            t.dataset.title = '复制失败'
+        }
+    }
+}
+var numbering = function () {
+    // 生成行号
+    $("pre code").each(function () {
+        $(this).append('<div class="hljs-button" data-title="复制"></div>');
+        var code = this.innerText.replace(/(^\s*)|(\s*$)/g, "");
+        $(this).bind('click', code_copy);
+        $(this).after('<ul class="pre-numbering" >');
+        var ul = $(this).next().get(0);
+        for (var i = 1; i <= code.split('\n').length; i++) {
+            ul.innerHTML += '<li style="color: rgb(153, 153, 153);">' + i + '</li>';
+        }
+    });
+}
+
 $(document).ready(function () {
+    var stackedit = new Stackedit();
     // 判断 hash
     switch (window.location.hash) {
         case '#main':
@@ -9,6 +55,48 @@ $(document).ready(function () {
             break;
 
     }
+    var intr = $('.display-main-introduction');
+    intr.html(HTMLDecode(intr.html()));
+    numbering();
+    intr.show();
+    $('#edit').on('click', function () {
+        var el = document.querySelector('.display-main-introduction');
+        var ta = document.querySelector('.display-main-middle .card textarea');
+        var old_html = el.innerHTML;
+        var old_text = ta.innerText;
+        stackedit.openFile({
+            name: '编辑个人介绍',
+            content: {
+                text: ta.value
+            }
+        });
+        stackedit.on('fileChange', (file) => {
+            el.innerHTML = file.content.html;
+            ta.value = file.content.text;
+        });
+        stackedit.on('close', () => {
+            var text = ta.value;
+            var html = el.innerHTML;
+            $.post('/user/modify/introduction', { 'md': text, 'html': html }, function (rel) {
+                if (rel == 'True') {
+                    numbering();
+                    return;
+                } else {
+                    el.innerText = '修改失败';
+                    ta.innerText = old_text;
+                    setTimeout(function () {
+                        el.innerHTML = old_html;
+                    }, 2500);
+                }
+            }).fail(function () {
+                el.text('修改失败');
+                ta.innerText = old_text;
+                setTimeout(function () {
+                    el.innerHTML = old_html;
+                }, 2500);
+            });
+        });
+    });
 
     $('.display-user-personalized').on('click', function () {
         $(this).hide();
@@ -30,7 +118,7 @@ $(document).ready(function () {
                     p.text('修改失败');
                     setTimeout(function () {
                         p.text(old_text);
-                    });
+                    }, 2500);
                 }
                 p.show();
             }).fail(function () {
@@ -38,7 +126,7 @@ $(document).ready(function () {
                 p.text('修改失败');
                 setTimeout(function () {
                     p.text(old_text);
-                });
+                }, 2500);
             });
         } else
             p.show();
