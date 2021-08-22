0c4f0858a6be5b58622a2c9f925761ea57536fd8
syhanjin <2819469337@qq.com>
2021-08-22 21:42:33 +0800

    tools/EL/test

--- handler/tool_EL.py
+++ handler/tool_EL.py
@@ -1,7 +1,132 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, request,jsonify,session,redirect
+import datetime
+import random
+import os
+from flask import Blueprint, render_template, request, jsonify, session, redirect
+from flask.helpers import send_file
+from pydub.audio_segment import AudioSegment
+path = os.path.join('.','tmp','EL')
+if not os.path.exists(path):
+    os.makedirs(path)
+path = os.path.join('.','tmp','EL_download')
+if not os.path.exists(path):
+    os.makedirs(path)
 
 EL = Blueprint('EL', __name__)
+
+def audio_add(tmp,id,R=False):
+    path = os.path.join('.','tmp','EL',id+'.mp3')
+    tmp.save(path)
+    tmp_au = AudioSegment.from_file(path,'mp3')
+    if R:
+        tmp_au += tmp_au
+    os.remove(path)
+    return tmp_au
+
+
 @EL.route('/tools/EL')
 def EL_home():
-  return render_template('tools/EL/pc/main.html')
+    return render_template('tools/EL/pc/main.html')
+
+
+@EL.route('/tools/EL/upload', methods=['POST'])
+def EL_upload():
+    id = datetime.datetime.now().__format__('%Y%m%d%H%M%S%f') + \
+            str(random.randint(10, 99))
+    au = AudioSegment.empty()
+    tmp = request.files.get('au-title')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id)
+    au += AudioSegment.silent(2000)
+    au += AudioSegment.from_mp3('static/tools/EL/01.mp3')
+    au += AudioSegment.silent(5000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt1')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt2')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt3')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt4')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt5')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(5000)
+    au += AudioSegment.from_mp3('static/tools/EL/02.mp3')
+    au += AudioSegment.from_mp3('static/tools/EL/03.mp3')
+    au += AudioSegment.silent(5000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt6')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/04.mp3')
+    au += AudioSegment.silent(1000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt7')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/05.mp3')
+    au += AudioSegment.silent(1000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt8')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/06.mp3')
+    au += AudioSegment.silent(1000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt9')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/07.mp3')
+    au += AudioSegment.silent(1000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt10')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id,True)
+    au += AudioSegment.silent(10000)
+    au += AudioSegment.from_mp3('static/tools/EL/08.mp3')
+    au += AudioSegment.silent(1000)
+    au += AudioSegment.from_mp3('static/tools/EL/dingdong.mp3')
+    tmp = request.files.get('au-pt11')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id)
+    tmp = request.files.get('au-pt12')
+    if not tmp:
+        return 'False'
+    au += audio_add(tmp,id)
+    path = os.path.join('.','tmp','EL_download',id+'.mp3')
+    au.export(path)
+    return id
+@EL.route('/tools/EL/download/<string:id>')
+def EL_download(id):
+    path = os.path.join('.','tmp','EL_download',id+'.mp3')
+    return send_file(path)
--- static/js/tools/EL/main.js
+++ static/js/tools/EL/main.js
@@ -1,32 +1,62 @@
 //alert("注意：需要获取录音权限！");
 
 var recorder = new Recorder({
-sampleRate: 44100,
-bitRate: 128,
-success: function(){
-},
-error: function(msg){
-},
-fix: function(msg){
-}
+    sampleRate: 44100,
+    bitRate: 128,
+    success: function () {
+    },
+    error: function (msg) {
+    },
+    fix: function (msg) {
+    }
 });
-$(document).ready(function() {
-    $("#main .part div input").click(function(e) {
+var fd = new FormData();
+$(document).ready(function () {
+    $("#main .part input").click(function (e) {
         if ($(this).val() == "录制" || $(this).val() == "重录") {
-            $("#main .part div input").attr("disabled", "disabled");
+            $("#main .part input").attr("disabled", "disabled");
             $(this).val("停止");
-            $("#recording").css({'display':'block'});
+            $("#recording").css({ 'display': 'block' });
             $(this).removeAttr("disabled");
-            setInterval(function(){
-                $("#recording em").text(recorder.duration);
-            },1000);
+            recorder.time = 0;
+            $("#recording em").text(0);
+            recorder.interval = setInterval(function () {
+                recorder.time++;
+                console.log(recorder.time);
+                $("#recording em").eq(0).text(parseInt(recorder.time / 60));
+                $("#recording em").eq(1).text(recorder.time % 60);
+            }, 1000);
             recorder.start();
-        }else{
-            $("#main .part div input").removeAttr("disabled");
-            $("#recording").css({'display':'none'});
+        } else {
+            $("#main .part input").removeAttr("disabled");
+            $("#recording").css({ 'display': 'none' });
             $(this).val("重录");
-            recorder.stop();
+            clearInterval(recorder.interval);
+            recorder.stop()
+            recorder.name = $(this).attr('name')
+            recorder.getBlob((blob) => {
+                fd.set(recorder.name, blob)
+                $(this).nextAll('audio').get(0).src = URL.createObjectURL(blob);
+            });
+
         }
     });
+    $('#upload').on('click', function () {
+
+        $.ajax({
+            url: '/tools/EL/upload',
+            type: 'post',
+            contentType:false,
+            processData:false,
+            data: fd,
+            success: function (rel) {
+                if (rel == 'False') {
+                    new Event('数据不全')
+                } else {
+                    window.location.href = "/tools/EL/download/" + rel;
+                }
+            }
+        })
+    })
 });
 
